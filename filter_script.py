1b496c983a62b510d4f0bdd0e7ddd5b23ad845e4
Your Name <your-email@example.com>
2026-02-11 14:42:27 +0200

    new

---
+++ filter_script.py
@@ -0,0 +1,8 @@
+***REMOVED***
+
+***REMOVED***
+***REMOVED***
+    content = re.sub(r'REDACTED_AWS_ACCESS_KEY', 'REDACTED_AWS_ACCESS_KEY', content)
+    content = re.sub(r'REDACTED_AWS_SECRET_KEY', 'REDACTED_AWS_SECRET_KEY', content)
+***REMOVED***
+***REMOVED***
